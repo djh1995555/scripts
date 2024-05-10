24061bf480ecce59ea2f7b8a29108a45798d0214
dongjianhao <dongjianhao@xiaomi.com>
2024-05-10 13:58:36 +0800

    replay: record more channels

--- record_replayer/record_replayer.py
+++ record_replayer/record_replayer.py
@@ -6,10 +6,29 @@ import sys
 import time
 
 def main(args):
-    target_channels = ['/parking/controller/command','/parking/controller/debug','/parking/controller/hmi_report','/parking/controller/status']
+    target_channels_blocked = ['/parking/controller/command','/parking/controller/debug','/parking/controller/hmi_report','/parking/controller/status']
+    record_target_channels = [
+      "/localization/kinetic_localization",
+      "/sensors/chassis/parking",
+      "/parking/apa/trajectory",
+      "/parking/avp/trajectory",
+      "/sensors/ultrasonic/echo",
+      "/parking/obstacle_map/apa",
+      "/hmi/parking/interact_command",
+      "/parking/apa/apa_map",
+      "/parking/avp/state_machine",
+      "/parking/apa/state_machine",
+      "/parking/apa_planner/status",
+      "/parking/apa_planner/hmi_report",
+      "/perception/prediction/obstacles/avp",
+      "/mcu/soc/carconfig_params",
+      "/parking/controller/command",
+      "/parking/controller/debug",
+      "/parking/controller/status",
+      "/parking/controller/hmi_report",
+    ]
     process_list = []
 
-    folder_path = '/path/to/your/folder'
 
     file_paths = list(os.walk(args.target_record_dir))
     target_record_list = []
@@ -20,34 +39,40 @@ def main(args):
 
     print(f"replay_record_filepath:{args.target_record_dir}")
     print(f"output_dir:{args.output_dir}")
+    print(f"target_record_list:{target_record_list}")
+
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir) 
+
+    play_cmd = ['recorder', 'play', '-f']
+    for target_record in target_record_list:
+        play_cmd.append(target_record) 
+    play_cmd.append('-k')
+    for target_channel in target_channels_blocked:
+        play_cmd.append(target_channel)  
+    if(args.start_time is not "00"):
+        play_cmd.append('-s')
+        play_cmd.append(str(args.start_time))
+    print(f"play_cmd=========={play_cmd}")
+    record_play_process = subprocess.Popen(play_cmd)
+    process_list.append(record_play_process)
+    time.sleep(1.9) 
 
     control_node_cmd = [f"{args.project_dir}/bazel-bin/mipilot/modules/parking/controller/app/node_launch_controller", 
                         f"{args.project_dir}/mipilot/conf/cross_platform/parking/controller/node_launch_controller_debug.pb.conf"]
     control_process = subprocess.Popen(control_node_cmd, cwd = args.project_dir)
     process_list.append(control_process)
 
-    if not os.path.exists(args.output_dir):
-        os.makedirs(args.output_dir) 
-    output_name = os.path.join(args.output_dir, "replay.record")
 
+    output_name = os.path.join(args.output_dir, "replay.record")
     # record_process = subprocess.Popen(['recorder', 'record', '-o', output_name, '-a',])
     record_cmd = ['recorder', 'record', '-o', output_name]
-    for target_channel in target_channels:
+    for target_channel in record_target_channels:
         record_cmd.append('-c')
         record_cmd.append(target_channel) 
     record_process = subprocess.Popen(record_cmd)
     process_list.append(record_process)
 
-
-    play_cmd = ['recorder', 'play', '-f']
-    for target_record in target_record_list:
-        play_cmd.append(target_record) 
-    play_cmd.append('-k')
-    for target_channel in target_channels:
-        play_cmd.append(target_channel)  
-    print(f"play_cmd=========={play_cmd}")
-    record_play_process = subprocess.Popen(play_cmd)
-    process_list.append(record_play_process)
 
     while(True):     
         time.sleep(1) 
@@ -64,7 +89,7 @@ if __name__ == '__main__':
     parser.add_argument('--target-record-dir', type=str)
     parser.add_argument('--output-dir', default=os.path.join(scripts_dirname,'record/replay_record/default_report'))
     parser.add_argument('--project-dir', default='/home/mi/tmp_repo_mipilot_mbf_1108_NEvh35')
-    parser.add_argument('--time', default='0000', type=str)
+    parser.add_argument('--start-time', default="00", type=str)
     args = parser.parse_args()
     
     main(args)
